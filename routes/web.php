8e65c34101a4c39bf76d491d16aaf13d31261b92
Eric Oriez <ericoriez@live.fr>
2024-07-10 15:07:10 +0200

    Route product{id}

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route pour la fiche détail d'un produit
+Route::get('/product/{id}', function ($id){
+    return "Fiche du produit $id";
+}
+
+);
