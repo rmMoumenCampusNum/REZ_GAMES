8595dd700e0fc5b8ba0532a4b6633a4505508aac
Moumen Bourahla <moumen.bourahla@le-campus-numerique.fr>
2024-07-12 14:24:21 +0200

    Ajout du controller order

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\OrdersConroller;
-use App\Http\Controllers\UserController;
-use app\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,49 +8,45 @@ Route::get('/', function () {
 });
 
 
-// ROUTE pour les Items / Johan
-Route::get('/items', function () {
-    return "La liste des Items";
+Route::get('/product', function () {
+    return 'La liste de produits';
 });
-//ASSIGNATION CONTROLLER Items /Johan
-Route::get('/items', [ItemController::class, 'index']);
 
 
 Route::get('/customer', function () {
-    return 'La liste des clients';
+    return ['Tableau' => 'La liste des clients'];
 });
 
-//assignation du controleur pour la route user, avec comme texte d'affichage si ok return ['Tableau' => 'La liste des clients'];
-Route::get('/user', [UserController::class, 'showAll']);
+Route::get('/customer/{id}', function($id){
+    return "Fiche du client $id";
 
+});
 
-// assignation du controleur pour la route user$id, avec comme texte d'affichage si ok ""User controller Ok with $id";
-Route::get('/user/{id}', [UserController::class, 'showOne']);
-
-
-
-// Route pour la fiche détail d'un produit.
+// Route pour la fiche détail d'un produit
 Route::get('/product/{id}', function ($id){
     return "Fiche du produit $id";
-});
-
-Route::get('/orders', [OrdersConroller::class, 'orders']);
-
-Route::get('/orders/{id}', function ($id) {
-    return "Fiche du commande id $id";
-});
-
-Route::get('/shipments', function () {
-    return "La liste des envoi";
-});
-
-Route::get('/shipments/{id}', function ($id) {
-    return "Fiche de l'envoi $id";
-});
+}
 
 );
 
+Route::get('/orders', [OrdersConroller::class, 'orders']);
 
-Route::get('/Categories', [\App\Http\Controllers\CategoriesController::class, 'show']);
+Route::get('/orders/{id}', function ($id){
+    return "Fiche du commande id $id";
+});
 
-Route::get('/Categories/{id}', [\App\Http\Controllers\CategoriesController::class, 'showId']);
+Route::get('/shipments', function (){
+    return "La liste des envoi";
+});
+
+Route::get('/shipments/{id}', function ($id){
+    return "Fiche de l'envoi $id";
+});
+
+Route::get('/categories', function (){
+   return "liste des catégories";
+});
+
+Route::get('/categories/{id}', function ($id){
+        return "Catégorie $id";
+});
