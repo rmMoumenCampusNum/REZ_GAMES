cc9cd1ef26d03cad49d89e88b5d2ad5964f27ad6
Johan <johan.bertrand@le-campus-numerique.fr>
2024-07-12 16:17:40 +0200

    route+controller Items

--- routes/web.php
+++ routes/web.php
@@ -1,58 +1,30 @@
 <?php
 
-use App\Http\Controllers\OrdersConroller;
-use App\Http\Controllers\UserController;
-use app\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// ROUTE pour les Items / Johan
-Route::get('/items', function () {
-    return "La liste des Items";
-});
+// route des produits items vers
+Route::get('/product', function (Request $request) {
+    return ['La liste de produits' => input('La liste des clients');
 
-//ASSIGNATION CONTROLLER Items /Johan
-Route::get('/items', [ItemController::class, 'index']);
+});
 
 
 Route::get('/customer', function () {
-    return 'La liste des clients';
+    return ['Tableau' => 'La liste des clients'];
 });
 
-//assignation du controleur pour la route user, avec comme texte d'affichage si ok return ['Tableau' => 'La liste des clients'];
-Route::get('/user', [UserController::class, 'showAll']);
+Route::get('/customer/{id}', function ($id) {
+    return "Fiche du client $id";
 
+});
 
-// assignation du controleur pour la route user$id, avec comme texte d'affichage si ok ""User controller Ok with $id";
-Route::get('/user/{id}', [UserController::class, 'showOne']);
-
-
-
-// Route pour la fiche détail d'un produit.
-Route::get('/product/{id}', function ($id){
+// Route pour la fiche détail d'un produit
+Route::get('/product/{id}', function ($id) {
     return "Fiche du produit $id";
-});
+}
 
-Route::get('/orders', [OrdersConroller::class, 'orders']);
-
-Route::get('/orders/{id}', function ($id) {
-    return "Fiche du commande id $id";
-});
-
-Route::get('/shipments', function () {
-    return "La liste des envoi";
-});
-
-Route::get('/shipments/{id}', function ($id) {
-    return "Fiche de l'envoi $id";
-});
-
-
-
-
-Route::get('/Categories', [\App\Http\Controllers\CategoriesController::class, 'show']);
-
-Route::get('/Categories/{id}', [\App\Http\Controllers\CategoriesController::class, 'showId']);
+);
