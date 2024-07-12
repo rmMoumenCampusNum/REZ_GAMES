da54a8c404e6f68d818b9255f0181172b12f157e
Johan <johan.bertrand@le-campus-numerique.fr>
2024-07-12 10:17:30 +0200

    route "La liste des produits"

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// route des produits items vers
+Route::get('/product', function (Request $request) {
+    return ['La liste de produits' => input('La liste des clients');
 
-Route::get('/product', function () {
-    return 'La liste de produits';
 });
 
 
@@ -16,13 +17,13 @@ Route::get('/customer', function () {
     return ['Tableau' => 'La liste des clients'];
 });
 
-Route::get('/customer/{id}', function($id){
+Route::get('/customer/{id}', function ($id) {
     return "Fiche du client $id";
 
 });
 
 // Route pour la fiche détail d'un produit
-Route::get('/product/{id}', function ($id){
+Route::get('/product/{id}', function ($id) {
     return "Fiche du produit $id";
 }
 
