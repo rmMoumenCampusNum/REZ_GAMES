c7525b7eecdf92a0d6daf09ee83a1676d985305a
RDelory74 <roro_spike@hotmail.com>
2024-07-15 16:57:11 +0200

    up shipment table

--- database/migrations/2024_07_15_113539_create_shipments_table.php
+++ database/migrations/2024_07_15_113539_create_shipments_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('shipments', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('orders_id')->constrained();
+            $table->foreignId('user_id')->constrained();
         });
     }
 
