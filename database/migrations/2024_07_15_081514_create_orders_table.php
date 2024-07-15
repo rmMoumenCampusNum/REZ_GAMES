b77859173e29e8e4b113a9e897aa2e85be890451
Moumen Bourahla <moumen.bourahla@le-campus-numerique.fr>
2024-07-15 11:25:19 +0200

    création d'un modèle et d'une migration Order

--- database/migrations/2024_07_15_081514_create_orders_table.php
+++ database/migrations/2024_07_15_081514_create_orders_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('user_id')->constrained();
+
         });
     }
 
