530c3b11ba0acc898e69cb3326eb51c604db4c63
Johan <johan.bertrand@le-campus-numerique.fr>
2024-07-15 14:33:55 +0200

    Créations des migrations Johan

---
+++ app/Models/Item.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Item;
+
+class Item extends Models
+{
+    public function ('home', ['Item' => Item::all()]){
+
+
+    return ('home', [
+    'Item' => Item::all()
+])}
+
+}
+
+
+
