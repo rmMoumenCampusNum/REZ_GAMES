5c79e037b9c829165a3ef6070c797133c64ba9bf
Moumen Bourahla <moumen.bourahla@le-campus-numerique.fr>
2024-07-15 12:19:15 +0200

    mon cul 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 use App\Http\Controllers\OrdersConroller;
+use App\Http\Controllers\UserController;
+use app\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
