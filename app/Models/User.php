103b3205e0e2aa8aa1d0a9542be672460c8e92e8
RDelory74 <roro_spike@hotmail.com>
2024-07-16 14:25:14 +0200

    up model card n user

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     use HasFactory;
+
+    protected $filable = ['title', 'decription', 'email'];
+,
+    public function Card()
+    {
+    return $this->belongsTo(Card::class);
+    }
 }
