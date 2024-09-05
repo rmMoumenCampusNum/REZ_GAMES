ec3472991a4da9cc7e62cbf309cd6df4f1cca1e6
RDelory74 <roro_spike@hotmail.com>
2024-09-05 12:10:52 +0200

    up again

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Détails de l\'utilisateur')
 
 @section('content')
-    <h1>{{ $user>name }}</h1>
+    <h1>{{ $user->name }}</h1>
     <p>{{ $user->email }}</p>
     <p>Password: {{ $user->password }}</p>
     <a href="{{ route('users.index') }}" class="btn btn-secondary">Retour à la liste</a>
